8aaadac9d7d00397445e047d07f1cc58d99dc73b
Nick Hodges <nickhodges@gmail.com>
2022-06-09 19:22:43 -0400

    Ignore config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,19 +10,11 @@ import { AuthModule } from './auth/auth.module';
 
 dotenv.config();
 
-// const options: Auth0Options = {
-//   domain: process.env.AUTH0_DOMAIN,
-//   clientId: process.env.AUTH0_CLIENT_ID,
-//   clientSecret: process.env.AUTH0_CLIENT_SECRET,
-//   audience: process.env.AUTH0_AUDIENCE,
-// };
-
 @Module({
   imports: [JwtModule, AuthModule],
   controllers: [AppController, CodeController, PersonnelController],
   providers: [
     AppService,
-
   ],
 })
 export class AppModule {}
